5b3d21521b4db90fac237ca0109dd09710880365
5yaaz <47409670+5yaaz@users.noreply.github.com>
2019-02-08 15:08:48 +0530

    Update integers.py

--- integers.py
+++ integers.py
@@ -1,7 +1,7 @@
-a1,b1=raw_input().split()
-a1=int(a1,)
-b1=int(b1,)
-for k in range(a1+2,b1):
-	if(k % 2!= 0):
-		k=int(k)
-		print k
+s9,s1=raw_input().split()
+s9=int(s9,)
+s1=int(s1,)
+for i in range(s9+1,s1+1):
+	if(i%2!=0):
+		i=int(i)
+		print i,
